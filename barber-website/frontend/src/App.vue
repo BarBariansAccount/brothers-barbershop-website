ec59f1752f3bdc38da03fa9fefba55cdc6c67206
Yousha <youshahyatoolla@gmail.com>
2022-10-05 21:59:07 -0400

    Added Footer

--- barber-website/frontend/src/App.vue
+++ barber-website/frontend/src/App.vue
@@ -4,15 +4,17 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
 
-  components: { Navbar },
+  components: { Navbar, Footer },
 
   data: () => ({
     //
