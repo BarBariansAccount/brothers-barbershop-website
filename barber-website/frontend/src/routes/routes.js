ee3bfa6648deb71c21c0e1da1411063a9451f293
Mohamed A <mohamed.abdul.dimbio@gmail.com>
2022-10-11 14:21:57 -0400

    WIP on Barbers management page

--- barber-website/frontend/src/routes/routes.js
+++ barber-website/frontend/src/routes/routes.js
@@ -3,11 +3,13 @@ import ProfilePage from "../profile/ProfilePage.vue";
 import AdminPage from "../admin/AdminPage.vue";
 import GalleryPage from "../gallery/GalleryPage.vue";
 import AppointmentPage from "../appointment/AppointmentPage.vue";
+import BarbersManagementPage from "../admin/BarbersManagementPage.vue"
 
 export default [
   { path: "/", component: HomePage },
   { path: "/profile", component: ProfilePage },
-  { path: "/admin", component: AdminPage },
+  { path: "/admin", component: AdminPage,  },
+  { path: "/BarbersManagement", component: BarbersManagementPage},
   { path: "/gallery", component: GalleryPage },
   { path: "/appointment", component: AppointmentPage },
 ];
