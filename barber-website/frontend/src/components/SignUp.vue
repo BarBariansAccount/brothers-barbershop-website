8286fe2ae3dee3c0b2eda2b9b7d22e08b79a3a3b
Yousha <youshahyatoolla@gmail.com>
2022-10-11 15:07:31 -0400

    Added Sign In and Sign Up UI

---
+++ barber-website/frontend/src/components/SignUp.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-row justify="center">
+    <v-dialog :v-model="signUpDialog" width="500px" height="650px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn text v-bind="attrs" v-on="on" depressed> Sign Up </v-btn>
+      </template>
+      <v-card height="650px" width="500px" color="#DBDDEF">
+        <v-card-title class="text-h4 justify-center"> Sign Up </v-card-title>
+        <v-row justify="center">
+          <v-col cols="8">
+            <v-text-field
+              label="Full Name"
+              placeholder="Full Name"
+              solo
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="8">
+            <v-text-field
+              label="Phone Number"
+              placeholder="Phone Number"
+              solo
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="8">
+            <v-text-field label="Email" placeholder="Email" solo></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="8">
+            <v-text-field
+              label="Password"
+              placeholder="Password"
+              solo
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="8">
+            <v-text-field
+              label="Confirm Password"
+              placeholder="Confirm Password"
+              solo
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-btn class="white--text" @click="signUp()" color="#5f6bba">
+            Sign Up
+          </v-btn>
+        </v-row>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    signUpDialog: false,
+  }),
+};
+</script>
+
+<style></style>
