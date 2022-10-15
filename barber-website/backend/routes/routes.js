26d2df423a97d438cde85658be3a9d6ff9f9c77e
AQA98 <abdulqadir199853@gmail.com>
2022-10-14 20:24:30 -0400

    Prevented the backened from crashing and added try catch to all the methods in backend

--- barber-website/backend/routes/routes.js
+++ barber-website/backend/routes/routes.js
@@ -16,17 +16,20 @@ router.get('/users',User.getusers);
 
 /*
 Takes --> {UserRole, Email, FirstName, LastName, Telephone, Password} As json 
-returns --> res.send('User already exists.') || res.status(201).send(`New user: ${FirstName} sucessfully created.`) || throws error
+returns --> res.status(200).send(`New user: ${FirstName} sucessfully created.`) 
+|| res.status(400).send('User already exists. Enter different phone number.');
+ || res.status(400).send('User Role can only be "Customer" OR "Admin" OR "Barber"');
 */
 router.post('/createUser', User.createUser); 
 
 /*
 Takes --> {Telephone, Password} As json 
-returns --> res.send(`There is no user with ${Telephone}.`) || res.send('Password is incorrect'); || JWT token if the authorization is sucessfull || throws error
+returns --> res.status(400).send(`There is no user with ${Telephone}.`); || res.status(400).send('Password is incorrect'); || JWT token if the authorization is sucessfull
 */
 router.post('/Login', User.validateLogin)
 
 /*
+Assumptions --> Telephone number cannot be changed or updated.
 Takes --> {Email, FirstName, LastName, Telephone, Password} As json 
 returns --> res.send(`This phone number is already associated with another account: ${Telephone}. Please try providing another phone number.`);
             || res.send(`This email is already associated with another account: ${Email}. Please try providing another Email.`);
@@ -37,7 +40,9 @@ router.post('/updateUser', User.updateUser);
 
 /*
 Takes --> {Telephone} As json 
-returns --> res.send(`User has been sucessfully deleted with: ${Telephone}`) || throws error
+returns --> res.status(200).send(`User has been sucessfully deleted with: ${Telephone}.`) 
+            || res.status(400).send(`There is no user with the number: ${Telephone}.`);
+            ||res.status(400).send(error)
 */
 router.post('/deleteUser', User.deleteUser);
 
