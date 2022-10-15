129e0da8fef0c63461c2e826a589ba43f4604d2e
Mohamed A <mohamed.abdul.dimbio@gmail.com>
2022-10-15 12:27:02 -0400

    getting all barbers and showing it up in the barbers management page

--- barber-website/frontend/src/admin/BarbersManagementPage.vue
+++ barber-website/frontend/src/admin/BarbersManagementPage.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container>
         <v-row>
-            <v-col lg="3" md="4" sm="6" xs="12" justify="space-around" v-for="item in menuItems" :key="item.title"
+            <v-col lg="3" md="4" sm="6" xs="12" justify="space-around" v-for="item in barbers" :key="item.index"
                 class="pa-10 mb-5">
                 <v-card width="300" class="">
-                    <v-img height="300px" src="../assets/noImage.jpg">
+                    <v-img height="300px" src="../assets/black.jpeg">
                         <v-app-bar flat color="rgba(0, 0, 0, 0)">
                             <v-spacer></v-spacer>
                             <v-menu bottom right>
@@ -20,10 +20,9 @@
                                 </v-list>
                             </v-menu>
                         </v-app-bar>
-                        <v-card-title class="white--text mt-15 mr-10">
-                            <v-spacer></v-spacer>
-                            <p class=" text-right">
-                                {{item.title}}
+                        <v-card-title class="white--text" style="margin-right: auto; margin-left: auto">
+                            <p style="margin-right: auto; margin-left: auto">
+                                {{ item }}
                             </p>
                         </v-card-title>
                     </v-img>
@@ -32,40 +31,52 @@
         </v-row>
         <v-row>
             <v-col cols="12" class="text-center">
-                <v-btn class="white--text mb-15" @click="bookNow()">
+                <v-btn @click="getAllBarbers()" class="white--text mb-15">
                     <SignUp> Add Account </SignUp>
                 </v-btn>
             </v-col>
         </v-row>
-
     </v-container>
 </template>
 
 <script>
 import SignUp from "../components/SignUp.vue";
+import axios from "axios";
 export default {
     components: { SignUp },
     name: "BarbersManagementPage",
     data() {
         return {
-            items: [
-                { title: 'User Profile' },
-                { title: 'Delete User' },
-            ],
+            items: [{ title: "User Profile" }, { title: "Delete User" }],
             offset: true,
-            menuItems: [
-                { title: "BARBERS", },
-                { title: "CLIENTS", },
-                { title: "APPOINTMENT" },
-                { title: "PHOTOS", },
-                { title: "PRODUCTS", },
-                { title: "QUESTIONS", },
-                { title: "OUR INFO", },
-                { title: "OUR INF", }
-            ],
-        }
-    }
-}
+            barbers: [],
+        };
+    },
+    methods: {
+        async getAllBarbers() {
+            try {
+                const data = await axios.get(`http://localhost:5001/users`);
+                var users = data.data;
+                this.barbers = this.extractBarbersFromUsersList(users);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        extractBarbersFromUsersList(user) {
+            var barberList = [];
+            for (var i = 0; i < user.length; i++) {
+                if (user[i].userrole == "Barber") {
+                    barberList.push(user[i].firstname);
+                } else continue;
+            }
+            return barberList;
+        },
+    },
+    created: function () {
+        this.getAllBarbers();
+    },
+};
 </script>
 
 <style>
