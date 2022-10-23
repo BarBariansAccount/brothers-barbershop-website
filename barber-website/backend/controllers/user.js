9b70683ab555e45ce2a596f87b03cce4d7a35f60
AQA98 <abdulqadir199853@gmail.com>
2022-10-23 14:37:13 -0400

    added await and async in users.js

--- barber-website/backend/controllers/user.js
+++ barber-website/backend/controllers/user.js
@@ -3,23 +3,18 @@ const UserModel = require("../models/UserModel.js")
 const bcrypt = require("bcrypt")
 const JWT = require("jsonwebtoken");
 
-
-// to get all users in the database
-const getusers = (req, res) => {
+const getusers = async (req, res) => {
     try {
-        pool.query(UserModel.getUsers, (err, results) => {
-            if (err) {
-                res.send(err)
-            }
-            res.status(200).json(results.rows);
+        let results = await pool.query(UserModel.getUsers)
 
-        })
+        res.status(200).json(results.rows);
+
     } catch (err) {
-        console.log(err)
+        res.status(400).send(err)
     }
 }
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
     const {
         UserRole,
         Email,
@@ -33,63 +28,48 @@ const createUser = (req, res) => {
 
     //checking if email already exists
     try {
-        pool.query(UserModel.checkUserExists, [Telephone], (error, result) => {
-            if (!(UserRole == "Customer" || UserRole == "Admin" || UserRole == "Barber")) {
-                res.status(400).send('User Role can only be "Customer" OR "Admin" OR "Barber"');
-            } else if (result.rows.length) {
-                res.status(400).send('User already exists. Enter different phone number.');
-            } else if (error) {
-                throw error;
-            } else {
-                //adding a new user
-                try {
-                    pool.query(UserModel.addUser, [UserRole, Email, FirstName, LastName, Telephone, hash], (err, results) => {
-                        if (err) {
-                            throw error;
-                        }
-                        res.status(200).send(`New user: ${FirstName} sucessfully created.`)
-                    })
-                } catch (err) {
-                    res.status(400).send(err);
-                }
-            }
-        })
-    } catch (error) {
-        console.log(error)
-    }
+        let result = await pool.query(UserModel.checkUserExists, [Telephone])
+        if (!(UserRole == "Customer" || UserRole == "Admin" || UserRole == "Barber")) {
+            res.status(400).send('User Role can only be "Customer" OR "Admin" OR "Barber"');
+        } else if (result.rows.length) {
+            res.status(400).send('User already exists. Enter different phone number.');
+        } else {
+            //adding a new user
+            result = await pool.query(UserModel.addUser, [UserRole, Email, FirstName, LastName, Telephone, hash])
 
+            res.status(200).send(`New user: ${FirstName} sucessfully created.`)
+        }
+    } catch (error) {
+        res.status(400).send(error)
+    }
 }
 
-const validateLogin = (req, res) => {
+const validateLogin = async (req, res) => {
     const {
         Telephone,
         Password
     } = req.body;
 
     try {
-        pool.query(UserModel.checkUserExists, [Telephone], (error, Results) => {
-            if (Results.rows.length == 0) {
-                res.status(400).send(`There is no user with ${Telephone}.`);
-            } else if (error) {
-                throw error;
-            } else if (Results.rows.length == 1) {
+        let Results = await pool.query(UserModel.checkUserExists, [Telephone])
+        if (Results.rows.length == 0) {
+            res.status(400).send(`There is no user with ${Telephone}.`);
+        } else if (Results.rows.length == 1) {
 
-                if (!bcrypt.compareSync(Password, Results.rows[0].password)) {
-                    res.status(400).send('Password is incorrect.');
-                }
-                if (bcrypt.compareSync(Password, Results.rows[0].password)) {
-
-                    res.status(200).json(Results.rows);
-                }
+            if (!bcrypt.compareSync(Password, Results.rows[0].password)) {
+                res.status(400).send('Password is incorrect.');
             }
-        })
-    } catch (error) {
-        console.log(error)
-    }
+            if (bcrypt.compareSync(Password, Results.rows[0].password)) {
 
+                res.status(200).json(Results.rows);
+            }
+        }
+    } catch (error) {
+        res.status(400).send(error)
+    }
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     const {
         Email,
         FirstName,
@@ -98,77 +78,52 @@ const updateUser = (req, res) => {
         Password
     } = req.body;
     try {
-        pool.query(UserModel.checkUserExists, [Telephone], (error, results) => {
-            if (results.rows.length == 0) {
-                res.status(400).send(`This phone number is not associated with any account: ${Telephone}. Please try providing another phone number.`);
-            } else if (error) {
-                throw error;
-            } else {
-                const hash = bcrypt.hashSync(Password, 12);
-                try {
-                    pool.query(UserModel.updateUser, [Email, FirstName, LastName, Telephone, hash], (err, results) => {
-                        
-                        if (err) {
-                            throw err;
-                        }
-                        
-                        res.status(200).send("Information has been updated.")
-                    })
-                } catch (err) {
-                    res.status(400).send(err);
-                }
+        let results = await pool.query(UserModel.checkUserExists, [Telephone]);
+        if (results.rows.length == 0) {
+            return res.status(400).send(`This phone number is not associated with any account: ${Telephone}. Please try providing another phone number.`);
+        }
+        const hash = bcrypt.hashSync(Password, 12);
+        results = await pool.query(UserModel.updateUser, [Email, FirstName, LastName, Telephone, hash]);
 
-            }
-        })
+        res.status(200).send("Information has been updated.");
     } catch (error) {
         res.status(400).send(error)
     }
 }
 
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
     const {
         Telephone
     } = req.body;
     try {
-        pool.query(UserModel.checkUserExists, [Telephone], (error, results) => {
-            if (results.rows.length == 0) {
-                res.status(400).send(`There is no user with the number: ${Telephone}.`);
-            } else if (error) {
-                throw error;
-            } else {
-                try {
-                    pool.query(UserModel.deleteUser, [Telephone], (err, result) => {
-                        if (err) {
-                            throw err;
-                        } else {
-                            res.status(200).send(`User has been sucessfully deleted with: ${Telephone}.`)
-                        }
-                    })
-                } catch (err) {
-                    res.status(400).send(err);
-                }
-            }
-        })
+        let results = await pool.query(UserModel.checkUserExists, [Telephone])
+
+        if (results.rows.length == 0) {
+            return res.status(400).send(`There is no user with the number: ${Telephone}.`);
+        }
+
+        results = await pool.query(UserModel.deleteUser, [Telephone]);
+
+        res.status(200).send(`User has been sucessfully deleted with: ${Telephone}.`)
 
     } catch (error) {
         res.status(400).send(error)
     }
 }
 
-const getUser = (req, res) => {
+const getUser = async (req, res) => {
     const {
         Telephone
     } = req.body;
     try {
-        pool.query(UserModel.checkUserExists, [Telephone], (error, results) => {
-            if (results.rows.length == 0) {
-                res.status(400).send(`There is no user with the number: ${Telephone}.`);
-            } else if (error) {
-                throw error;
-            } else {
-                res.status(200).json(results.rows)
-            }
-        })
+        let results = await pool.query(UserModel.checkUserExists, [Telephone])
+
+        if (results.rows.length == 0) {
+            return res.status(400).send(`There is no user with the number: ${Telephone}.`);
+        } else {
+            res.status(200).send(results.rows)
+        }
+
     } catch (error) {
         res.status(400).send(error)
     }
