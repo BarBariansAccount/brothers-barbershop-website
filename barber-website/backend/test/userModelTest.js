26654e3173a012c7d63c02f77b750c2cfd548ab5
HWJFish <wwc641810324@gmail.com>
2022-10-27 01:20:10 -0400

    update and fixed latest change to user

--- barber-website/backend/test/userModelTest.js
+++ barber-website/backend/test/userModelTest.js
@@ -3,49 +3,80 @@ const {
     createUser,
     validateLogin,
     updateUser,
+    updatePassword,
     deleteUser,
     getUser
 } = require("../controllers/user")
 const { assert } = require('chai')
-const sinon = require('sinon')
 const { mockRequest, mockResponse, sleep } = require('./commonTestingMethods')
 
 
 describe("UserController related Test", function () {
+    const userData = {
+        UserRole: 'Customer',
+        Email: 'unitTesing@gmail.com',
+        FirstName: 'UnitFirst',
+        LastName: 'UnitLast',
+        Telephone: 5555555555,
+        Password: 'testPassword'
+    }
+    const updateData = {
+        Email: 'unitTesing@gmail.com',
+        FirstName: 'UnitFirst1',
+        LastName: 'UnitLast',
+        Telephone: 5555555555,
+        Password: 'testPassword'
+    }
     it("test user related test", async function () {
-        const userData = {
-            UserRole: 'Customer',
-            Email: 'unitTesing@gmail.com',
-            FirstName: 'UnitFirst',
-            LastName: 'UnitLast',
-            Telephone: 5555555555,
-            Password: 'testPassword'
-        }
-        const updateData = {
-            Email: 'unitTesing@gmail.com',
-            FirstName: 'UnitFirst1',
-            LastName: 'UnitLast',
-            Telephone: 5555555555,
-            Password: 'testPassword1'
-        }
+
+
 
         let req = mockRequest(userData);
         let res = mockResponse();
 
         await createUser(req, res);
         assert.equal(res.status.calledWith(200), true);
+
+        res = mockResponse();
         await validateLogin(req, res);
+
         assert.equal(res.status.calledWith(200), true);
+
+        let UserID = res.json.getCall(0).args[0][0].userid;
+        userData.UserID = UserID;
+        updateData.UserID = UserID;
+
+        const updatePasswordData = {
+            OldPassword: userData.Password,
+            NewPassword: 'modifiedPass',
+            UserID
+        }
+        const modifiedPasswordData = {
+            Telephone: updateData.Telephone,
+            Password: updatePasswordData.NewPassword
+        }
+
+        res = mockResponse();
         await updateUser(mockRequest(updateData), res);
         assert.equal(res.status.calledWith(200), true);
 
+        res = mockResponse();
         await getUser(req, res);
-        assert.equal(res.json.calledWith(), true);
+        assert.equal(res.send.getCall(0).args[0][0].firstname, updateData.FirstName);
+
+        res = mockResponse();
+        await updatePassword(mockRequest(updatePasswordData), res);
+        assert.equal(res.status.calledWith(200), true);
+
+        res = mockResponse();
+        await validateLogin(mockRequest(modifiedPasswordData), res);
+        assert.equal(res.status.calledWith(200), true);
+
+
+        res = mockResponse();
         await deleteUser(req, res);
         assert.equal(res.status.calledWith(200), true);
 
-        //console.log(res.status.getCall())
 
-        //assert.equal(true, res.send.calledWith("Information has been updated."))
     })
 })
