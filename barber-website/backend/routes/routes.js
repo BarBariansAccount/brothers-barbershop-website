28dc6ff080c846807ecafe358e69a7bcd39b45f2
AQA98 <65059336+AQA98@users.noreply.github.com>
2022-10-29 15:21:58 -0400

    Update routes.js

--- barber-website/backend/routes/routes.js
+++ barber-website/backend/routes/routes.js
@@ -16,7 +16,7 @@ function authenticateToken(req,res,next){
         if (err){
            return res.status(403).send("Please Login again.");
         }
-        req.userId=Logged_userId
+       
         next()
     })
 }
