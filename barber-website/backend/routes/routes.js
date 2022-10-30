a3c323172876e1a928e374bbfbfbeefa47d7884b
MiraliChavoshi <mirali.chavoshi.edu@gmail.com>
2022-10-30 18:52:01 -0400

    FAQ

--- barber-website/backend/routes/routes.js
+++ barber-website/backend/routes/routes.js
@@ -3,36 +3,37 @@ const express = require("express");
 const router = express.Router();
 const User = require("../controllers/user.js");
 const BusyStatus = require("../controllers/BusyStatus.js");
+const FAQ = require("../controllers/FAQ.js")
 
 const JWT = require("jsonwebtoken");
 
 
-function authenticateToken(req,res,next){
-    const authHeader=req.headers['authorization'];
-    const token= authHeader && authHeader.split(' ')[1]
-    if (token== null) {
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) {
         return res.status(401).send('Send Token Please!')
     }
-    JWT.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,Logged_userId)=>{
-        if (err){
-           return res.status(403).send("Please Login again.");
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, Logged_userId) => {
+        if (err) {
+            return res.status(403).send("Please Login again.");
         }
 
         next()
     })
 }
 
-router.get('/',(req,res) =>{
-        res.send({
-            message: "hello"
-        })
+router.get('/', (req, res) => {
+    res.send({
+        message: "hello"
     })
+})
 
 
 router.get("/", (req, res) => {
-  res.send({
-    message: "hello",
-  });
+    res.send({
+        message: "hello",
+    });
 });
 
 
@@ -40,7 +41,7 @@ router.get("/", (req, res) => {
 To get all the users in users table Mainly for testing.
 */
 
-router.get('/users',authenticateToken,User.getusers);
+router.get('/users', authenticateToken, User.getusers);
 
 
 /*
@@ -50,8 +51,8 @@ returns --> res.status(200).send(`New user: ${FirstName} sucessfully created.`)
  || res.status(400).send('User Role can only be "Customer" OR "Admin" OR "Barber"');
 */
 
-router.post('/createUser',authenticateToken, User.createUser); //FOR ADMINS TO CREATE BARBERS ACCOUNTS
-router.post('/createUser_customers',User.createUser);//FOR CUSTOMERS
+router.post('/createUser', authenticateToken, User.createUser); //FOR ADMINS TO CREATE BARBERS ACCOUNTS
+router.post('/createUser_customers', User.createUser);//FOR CUSTOMERS
 
 
 /*
@@ -73,7 +74,7 @@ returns --> res.send(User not exists.);
 */
 
 
-router.post('/updateUser' ,authenticateToken, User.updateUser);
+router.post('/updateUser', authenticateToken, User.updateUser);
 
 
 
@@ -86,7 +87,7 @@ returns --> res.status(200).send(`User has been sucessfully deleted with User ID
             ||res.status(400).send(error)
 */
 
-router.post('/deleteUser',authenticateToken, User.deleteUser);
+router.post('/deleteUser', authenticateToken, User.deleteUser);
 
 
 /*
@@ -95,7 +96,7 @@ returns -->  res.status(400).send(`There is no user with this user ID: ${UserID}
              || res.status(200).json(results.rows) -->{userid, userrole, email, firstname, lastname} As json;
              || res.status(400).send(error)
 */
-router.get('/getUser',authenticateToken, User.getUser);
+router.get('/getUser', authenticateToken, User.getUser);
 
 
 
@@ -107,7 +108,7 @@ returns -->  res.status(400).send(`There is no user with this user ID: ${UserID}
              || res.status(400).send(error);
 */
 
-router.post('/updatePassword',authenticateToken,User.updatePassword);
+router.post('/updatePassword', authenticateToken, User.updatePassword);
 
 /*
 *****
@@ -123,7 +124,7 @@ returns --> res.status(200).send("Busy");
 
 */
 
-router.get('/getStatus',authenticateToken,BusyStatus.getStatus);
+router.get('/getStatus', authenticateToken, BusyStatus.getStatus);
 
 
 /*
@@ -135,7 +136,20 @@ takes --> {"Status": "Busy"}
             ||res.status(200).send("Status is set to: Not Busy");
             ||res.status(200).send("Status is set to: Empty");
 */
-router.post('/updateStatus',authenticateToken, BusyStatus.updateStatus);
+router.post('/updateStatus', authenticateToken, BusyStatus.updateStatus);
+
+/*
+*****
+FAQ
+*****
+*/
+
+/* To get all the FAQ in FAQ  table .*/
+
+router.get('/getFAQ', authenticateToken, FAQ.getFAQ);
 
 
+router.post('/addFAQ', authenticateToken, FAQ.addFAQ);
+router.post('/updateFAQ', authenticateToken, FAQ.updateFAQ);
+router.post('/deleteFAQ', authenticateToken, FAQ.deleteFAQ);
 module.exports = router;
