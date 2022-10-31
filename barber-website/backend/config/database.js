651be92350b7229a8758879cad6e82835a67d06c
Tripe2000 <tripe2000@gmail.com>
2022-10-31 15:17:09 -0400

    switched back to localhost

--- barber-website/backend/config/database.js
+++ barber-website/backend/config/database.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  host: "0.0.0.0",
+  host: "localhost",
   database: "barbershop",
   password: "POSTGRES",
   port: 5432,
