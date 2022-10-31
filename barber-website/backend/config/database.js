e9cfcaecfd0c6a89ea2c8d455efcece96499c6ec
Tripe2000 <tripe2000@gmail.com>
2022-10-31 15:05:02 -0400

    changed database address

--- barber-website/backend/config/database.js
+++ barber-website/backend/config/database.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
+  host: "0.0.0.0",
   database: "barbershop",
   password: "POSTGRES",
   port: 5432,
