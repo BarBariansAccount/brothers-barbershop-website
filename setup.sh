dbcefd02af486e8d837c68bed2aead8036859b17
Tripe2000 <tripe2000@gmail.com>
2022-10-31 15:59:34 -0400

    switched containers network back to virtual network

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ docker build -f backend-dockerfile -t barbershop:backend .
 docker build -f frontend-dockerfile -t barbershop:frontend .
 
 # Run containers
-docker run --name barbershop-db -v "$PWD/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop -p 5432:5432 --network host -d postgres
+docker run --name barbershop-db -v "$PWD/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop -p 5432:5432 --network barber-network -d postgres
 
 # Remove the -p argument from the backend when deploying, it is only for testing
-docker run --name barbershop-backend -p 5001:5001 --network host -d barbershop:backend 
-docker run --name barbershop-frontend -p 9001:9001 --network host -d barbershop:frontend 
+docker run --name barbershop-backend -p 5001:5001 --network barber-network -d barbershop:backend 
+docker run --name barbershop-frontend -p 9001:9001 --network barber-network -d barbershop:frontend 
