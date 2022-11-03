bb23767b3c8dc91292a384d3b5e89be1d7d2587d
Tripe2000 <tripe2000@gmail.com>
2022-11-03 16:44:55 -0400

    more file cleanup in ci-cd folder

--- setup.sh
+++ setup.sh
@@ -10,13 +10,13 @@ docker network rm barber-network
 docker network create barber-network
 
 # Build containers
-docker build -f ci-cd/backend-dockerfile -t barbershop:backend .
-docker build -f ci-cd/frontend-dockerfile -t barbershop:frontend .
+docker build -f ci-cd/docker/backend-dockerfile -t barbershop:backend .
+docker build -f ci-cd/docker/frontend-dockerfile -t barbershop:frontend .
 
 # Run containers
 # -e POSTGRES_HOST_AUTH_METHOD=md5
 # We can't have the password hard-coded here, this needs to change
-docker run --name barbershop-db -v "$PWD/ci-cd/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -v "$PWD/ci-cd/postgres.conf":/etc/postgresql/postgresql.conf -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop -p 5432:5432 --network barber-network -d postgres:alpine -c 'config_file=/etc/postgresql/postgresql.conf'
+docker run --name barbershop-db -v "$PWD/ci-cd/database/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -v "$PWD/ci-cd/database/postgres.conf":/etc/postgresql/postgresql.conf -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop -p 5432:5432 --network barber-network -d postgres:alpine -c 'config_file=/etc/postgresql/postgresql.conf'
 
 # Remove the -p argument from the backend when deploying, it is only for testing
 docker run --name barbershop-backend -p 5001:5001 --network barber-network -d barbershop:backend 
