7aec0bd19ae40d9b535367a70eb0e19cc45b223b
Tripe2000 <tripe2000@gmail.com>
2022-11-04 16:12:33 -0400

    changed docker containers network to host instead of bridge

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ docker rm -f barbershop-db
 docker network rm barber-network
 
 # Setup network
-docker network create barber-network
+# docker network create barber-network
 
 # Build containers
 docker build -f ci-cd/docker/backend-dockerfile -t barbershop:backend .
@@ -16,8 +16,8 @@ docker build -f ci-cd/docker/frontend-dockerfile -t barbershop:frontend .
 # Run containers
 # -e POSTGRES_HOST_AUTH_METHOD=md5
 # We can't have the password hard-coded here, this needs to change
-docker run --name barbershop-db -v "$PWD/ci-cd/database/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -v "$PWD/ci-cd/database/postgres.conf":/etc/postgresql/postgresql.conf -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop -p 5432:5432 --network barber-network -d postgres:alpine -c 'config_file=/etc/postgresql/postgresql.conf'
+docker run --name barbershop-db -v "$PWD/ci-cd/database/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -v "$PWD/ci-cd/database/postgres.conf":/etc/postgresql/postgresql.conf -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop -p 5432:5432 --network host -d postgres:alpine -c 'config_file=/etc/postgresql/postgresql.conf'
 
 # Remove the -p argument from the backend when deploying, it is only for testing
-docker run --name barbershop-backend -p 5001:5001 --network barber-network -d barbershop:backend 
-docker run --name barbershop-frontend -p 9001:9001 --network barber-network -d barbershop:frontend 
+docker run --name barbershop-backend -p 5001:5001 --network host -d barbershop:backend 
+docker run --name barbershop-frontend -p 9001:9001 --network host -d barbershop:frontend 
