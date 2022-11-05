2401c79094d6ee9aa85ce2d135f340cb3435636d
HWJFish <wwc641810324@gmail.com>
2022-11-05 03:09:00 -0400

    add create barber test

--- barber-website/frontend/cypress/e2e/spec.cy.js
+++ barber-website/frontend/cypress/e2e/spec.cy.js
@@ -35,35 +35,56 @@ describe('Test Links', () => {
     FirstName: 'BarberFirst',
     LastName: 'BarberLast'
   }
-
-
-  it('Login to admin account and log out', () => {
+  const loginAccount = (account) => {
     cy.visit('/');
 
     cy.get('.v-toolbar__content > [role="button"]').click();
     cy.get('.row > .v-btn').click();
-    cy.get('#input-93').type(TestAdminInfo.Telephone);
-    cy.get('.v-text-field__slot>[type="password"]').type(TestAdminInfo.Password);
+    cy.get(':nth-child(2) > .col > .v-input > .v-input__control > .v-input__slot').type(account.Telephone);
+    cy.get('.v-text-field__slot>[type="password"]').type(account.Password);
     cy.get('.mt-8').click();
+  }
+
+  // a function to fill the form 
+  const completeField = (nth, input) => {
+    cy.get(`:nth-child(${nth}) > .col > .v-input > .v-input__control > .v-input__slot`)
+      .type(input);
+  }
 
 
+  it('Login to admin account and log out', () => {
+
+    loginAccount(TestAdminInfo);
+    cy.wait(1000)
+
+    cy.get('.v-toolbar__content > [role="button"]').click();
+
+
+    cy.get('.v-list-item').contains("Log Out").click();
 
 
 
   })
 
 
-  // it('UC 27, 36 Create Barber account', () => {
-  //   //cy.visit('/admin');
-  // })
+  it('UC 27, 36 Create Barber account', () => {
+
+    loginAccount(TestAdminInfo);
+    cy.get('[href="/admin"] > .v-btn__content').click();
+    cy.get(':nth-child(2) > .d-flex > .row > .col-sm-12').click();
+    cy.get('button>span').contains("Add Account").click();
+    completeField(2, TestBarberInfo.FirstName);
+    completeField(3, TestBarberInfo.LastName);
+    completeField(4, TestBarberInfo.Telephone);
+    completeField(5, TestBarberInfo.email);
+    completeField(6, TestBarberInfo.Password);
+    completeField(7, TestBarberInfo.Password);
+    cy.get(':nth-child(8)>button').contains('Add Account').click();
 
 
-  // it('logout admin account', () => {
-  //   //cy.visit('/');
-  //   cy.get('.v-toolbar__content > [role="button"]').click();
+  })
 
-  //   // cy.get('.v-list-item').contains("Log Out").click();
-  // })
+
 
 
 })
