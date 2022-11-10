f689bb9abb61c84669b865bcfe9cc39fd9209f4d
Tripe2000 <tripe2000@gmail.com>
2022-11-10 12:40:25 -0500

    uc209 added dockerfile for testing backend

--- setup-testing.sh
+++ setup-testing.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Clean up old docker containers and images
+# Clean up old docker containers and networks
 docker rm -f $(docker ps -aq)
+docker network rm barber-network
 
 # Setup network
 docker network create barber-network
 
 # Build containers
-docker build -f backend-dockerfile -t barbershop:backend ci-cd/docker
-docker build -f frontend-dockerfile -t barbershop:frontend ci-cd/docker
+docker build -t barbershop:backend -f "ci-cd/docker/backend-testing-dockerfile" .
+# docker build -f frontend-dockerfile -t barbershop:frontend ci-cd/docker
 
-# Run containers
+# Run database containers
 docker run --name barbershopdb -v "$PWD/ci-cd/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop --net barber-network -d postgres
 
-# Remove the -p argument from the backend when deploying, it is only for testing
+# Run app containers
 docker run -p 5001:5001 --net barber-network -d barbershop:backend 
-docker run -p 8080:8080 --net barber-network -d barbershop:frontend 
+# docker run -p 8080:8080 --net barber-network -d barbershop:frontend
