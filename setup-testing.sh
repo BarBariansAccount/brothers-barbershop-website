ae355cdfc56d3b6e4214b86e8b5bb7a671f65e78
Tripe2000 <tripe2000@gmail.com>
2022-11-10 13:43:46 -0500

    uc209 got the testing code to connect with the database

--- setup-testing.sh
+++ setup-testing.sh
@@ -12,8 +12,11 @@ docker build -t barbershop:backend -f "ci-cd/docker/backend-testing-dockerfile" 
 # docker build -f frontend-dockerfile -t barbershop:frontend ci-cd/docker
 
 # Run database containers
-docker run --name barbershopdb -v "$PWD/ci-cd/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop --net barber-network -d postgres
+docker run --name database -v "$PWD/ci-cd/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -e POSTGRES_PASSWORD=November199853@ -e POSTGRES_DB=barbershop --net barber-network -d postgres
+
+# Sleep for a bit to let the databse initialize
+sleep 3
 
 # Run app containers
-docker run -p 5001:5001 --net barber-network -d barbershop:backend 
-# docker run -p 8080:8080 --net barber-network -d barbershop:frontend
+docker run -p 5001:5001 --net barber-network --name backend -d barbershop:backend 
+# docker run -p 8080:8080 --net barber-network --name frontend -d barbershop:frontend
