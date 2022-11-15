825e4b1f5041f2f09f5004cd549bef37c581cb07
Tripe2000 <tripe2000@gmail.com>
2022-11-15 15:34:20 -0500

    fixed host configuration for drone server

--- ci-cd/drone/reset.sh
+++ ci-cd/drone/reset.sh
@@ -9,7 +9,7 @@ docker run \
   --env=DRONE_GITHUB_CLIENT_ID=caa70262c30e167f4e40 \
   --env=DRONE_GITHUB_CLIENT_SECRET=0e6d5b163c0c325034f31f71eb79fd69ced2dbea \
   --env=DRONE_RPC_SECRET=ea26053a42cbf18f7843065281583453 \
-  --env=DRONE_SERVER_HOST=loop.encs.concordia.ca \
+  --env=DRONE_SERVER_HOST=loop.encs.concordia.ca:8080 \
   --env=DRONE_SERVER_PROTO=http \
   --env=DRONE_LOGS_DEBUG=true \
   --env=DRONE_USER_CREATE=username:Tripe2000,admin:true \
