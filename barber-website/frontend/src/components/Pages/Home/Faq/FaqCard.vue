bad8b3a322e8d6cb0b0fbf3fde2e265796849f95
Mohamed A <mohamed.abdul.dimbio@gmail.com>
2022-11-15 21:32:44 -0500

    UC-176 WIP in FAQ edit

--- barber-website/frontend/src/components/Pages/Home/Faq/FaqCard.vue
+++ barber-website/frontend/src/components/Pages/Home/Faq/FaqCard.vue
@@ -1,22 +1,26 @@
 <template>
-  <v-card class="mx-auto rounded-xl" color="#F7F7F7">
-    <v-card-text class="text-center">
-      <p
-        contentEditable="True"
-        class="my-2 text-center font-weight-bold text-h5"
-      >
-        <slot></slot>
-      </p>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="4" class="" v-for="faq in faqList" :key="faq.faqid">
+        <v-card
+          class="mx-auto rounded-xl"
+          color="#F7F7F7"
+          max-height="500px"
+          height="300"
+        >
+          <v-card-text class="text-center">
+            <p class="my-2 text-center font-weight-bold text-h5">
+              {{ faq.question }}
+            </p>
 
-      <div
-        contentEditable="True"
-        class="text-center mt-6 text-h6"
-        @click="getFAQ()"
-      >
-        {{}}
-      </div>
-    </v-card-text>
-  </v-card>
+            <div contentEditable="True" class="text-center mt-6 text-h6">
+              {{ faq.answer }}
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 import SavedFAQs from "@/services/user";
@@ -25,6 +29,7 @@ export default {
   data: () => ({
     loading: false,
     selection: 1,
+    faqsDict: {},
     faqList: [],
   }),
 
@@ -39,13 +44,20 @@ export default {
         console.log("Getting the list of FAQs");
         const response = await SavedFAQs.getAllFaqs();
         console.log("retrieved the FAQs");
-        var faqData = response.data[0];
-        this.faqList.push(response.data[0]);
-        console.log(faqData.answer);
+        this.getTheFAQsValues(response.data);
+        console.log(this.faqList);
       } catch (error) {
         console.log(error);
       }
     },
+    getTheFAQsValues(faqData) {
+      for (var i = 0; i < faqData.length; i++) {
+        this.faqList.push(faqData[i]);
+      }
+    },
+  },
+  created() {
+    this.getFAQ();
   },
 };
 </script>
