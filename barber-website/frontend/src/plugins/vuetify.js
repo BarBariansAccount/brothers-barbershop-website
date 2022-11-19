80da491ab5461b6927d8a98d3220aa33c0c8e98a
Mohamed A <mohamed.abdul.dimbio@gmail.com>
2022-11-18 22:05:48 -0500

    added icon

--- barber-website/frontend/src/plugins/vuetify.js
+++ barber-website/frontend/src/plugins/vuetify.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-
+import CustomPlusIcon from "@/components/Pages/Home/Faq/PlusIconComponent.vue";
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    values: {
+      custom: { // name of our custom icon
+        component: CustomPlusIcon, // our custom component
+      },
+    },
+  }
   // theme: {
   //     themes: {
   //       light: {
