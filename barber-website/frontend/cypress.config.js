45ca30b4d07257ae952741f8462977c1e70f634f
Tripe2000 <tripe2000@gmail.com>
2022-12-02 18:49:21 -0500

    uc218 changed cypress configs for troubleshooting

--- barber-website/frontend/cypress.config.js
+++ barber-website/frontend/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   projectId: 'vij8ht',
   e2e: {
     baseUrl: 'http://frontend:8080',
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
