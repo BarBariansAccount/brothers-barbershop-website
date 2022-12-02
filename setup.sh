c789bbfefa14c18dad4a315e8e319576d1b33068
Tripe2000 <tripe2000@gmail.com>
2022-12-02 15:21:29 -0500

    uc218 updated setup script to reflex current pipeline process

--- setup.sh
+++ setup.sh
@@ -11,8 +11,9 @@ docker build -t barbershop:backend --target production -f "ci-cd/docker/dockerfi
 docker build -t barbershop:frontend --target production -f "ci-cd/docker/dockerfiles/frontend-dockerfile" .
 
 # Run database
-docker run --name database -v "$PWD/ci-cd/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -e POSTGRES_PASSWORD=$1 -e POSTGRES_DB=barbershop --net barber-network -d postgres
+docker run --name database -v "$PWD/ci-cd/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh -e POSTGRES_PASSWORD=$1 -e POSTGRES_DB=barbershop -p 5432:5432 --net barber-network -d postgres
+sleep 3
 
 # Run containers
 docker run -p 5001:5001 --net barber-network --name backend -d barbershop:backend 
-docker run -p 9001:8080 --net barber-network --name frontend -d barbershop:frontend
+docker run -p 7070:8080 --net barber-network --name frontend -d barbershop:frontend
