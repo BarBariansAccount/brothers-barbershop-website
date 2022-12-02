a147475e6b17fd29e9f20518e0eb959e5649eaec
Tripe2000 <tripe2000@gmail.com>
2022-12-02 17:37:30 -0500

    uc218 changed port on cypress configs for troubleshooting

--- barber-website/frontend/cypress.config.js
+++ barber-website/frontend/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'vij8ht',
   e2e: {
-    baseUrl: 'http://frontend:7070',
+    baseUrl: 'http://frontend:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
