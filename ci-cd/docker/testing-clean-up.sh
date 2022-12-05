c32dd383b14964488adf9cd124647dc4ecda9f2d
Tripe2000 <tripe2000@gmail.com>
2022-12-05 15:02:35 -0500

    uc218 testing cleanup script now removes cypress container as well

--- ci-cd/docker/testing-clean-up.sh
+++ ci-cd/docker/testing-clean-up.sh
@@ -2,6 +2,7 @@
 
 echo "Note: It's okay if this fails, it's only clean up commands"
 docker rm $(docker ps --filter status=exited -q)
+docker rm -f cypress
 docker rm -f database
 docker rm -f backend-test
 docker rm -f frontend-test
