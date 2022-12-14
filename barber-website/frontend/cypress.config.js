a9a5f47907550405b40e6597338ac70164838760
Tripe2000 <tripe2000@gmail.com>
2022-12-14 16:04:10 -0500

    uc218 added slash at the end of cypress baseurl configs

--- barber-website/frontend/cypress.config.js
+++ barber-website/frontend/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'vij8ht',
   e2e: {
-    baseUrl: 'http://frontend:8080',
+    baseUrl: 'http://frontend:8080/',
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
