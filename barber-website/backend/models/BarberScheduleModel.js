c7eda139bd89f66726969138a2989425586b24aa
AQA98 <abdulqadir199853@gmail.com>
2022-12-19 20:41:05 -0500

    UC-46 added the get api (barber login page) to see all the information regarding the booked and unbooked appointments.

--- barber-website/backend/models/BarberScheduleModel.js
+++ barber-website/backend/models/BarberScheduleModel.js
@@ -1,8 +1,12 @@
 const checkAvailablityExists= "Select appointment_id FROM barber_schedule Where barber_name=$1 AND Available_Date=$2 AND hour=$3"
+
 const addAvailablity="INSERT INTO barber_schedule (UserID, barber_name,Available_Date,hour) VALUES($1, $2, $3, $4)"
+
+const getBarberAvailablity_barberView= "SELECT * FROM barber_schedule WHERE UserID=$1 AND Available_Date=$2 ORDER BY hour" 
 
 module.exports={
     checkAvailablityExists,
     addAvailablity,
+    getBarberAvailablity_barberView
 }
 
