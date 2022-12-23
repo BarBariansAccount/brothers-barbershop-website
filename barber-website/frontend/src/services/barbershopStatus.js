f71e9e9cd5e7814a182800759d2982fbf5e3067e
Yousha <youshahyatoolla@gmail.com>
2022-12-22 21:14:43 -0500

    UC-233 FIxed api update issue for barbershop status

--- barber-website/frontend/src/services/barbershopStatus.js
+++ barber-website/frontend/src/services/barbershopStatus.js
@@ -2,7 +2,7 @@ import api from "./http";
 
 export default new (class BarberShopStatus {
   updateStatus = (payload) => {
-    return api.post("/BusyStatus/updateStatus", payload);
+    return api.put("/BusyStatus/updateStatus", payload);
   };
   getStatus = () => {
     return api.get("/BusyStatus/getStatus");
