0e727ab6d5c319af7bb4a822d59caf163d72d0a5
AQA98 <abdulqadir199853@gmail.com>
2023-01-18 18:46:41 -0500

    UC-51 Add appointment route, which will also send email to the customer and also redirects the customer to the appointment info page

--- barber-website/backend/controllers/Appointment.js
+++ barber-website/backend/controllers/Appointment.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const pool = require('../config/database.js')
-const AppointmentModel = require("../models/AppointmentModel.js")
+const AppointmentModel = require("../models/AppointmentModel.js");
+const JWT = require("jsonwebtoken");
+const sendgrid = require("@sendgrid/mail");
 
 const BarberAvailablityDates= async (req,res) =>{
     const {
@@ -35,8 +37,56 @@ const getBarberAvailablity_Hours= async (req,res)=>{
         res.status(400).send(error)
     }
 }
+const addAppointment = async (req,res)=>{
+    const {
+        appointment_id,
+        Customer_First_name,
+        Customer_Last_name,
+        Customer_email,
+        Customer_telephone,
+        service,
+        Customer_appointment_notes
+    } = req.body;
+    try{
+        await pool.query(AppointmentModel.addAppointment,[appointment_id,Customer_First_name, Customer_Last_name, Customer_email, Customer_telephone, service, Customer_appointment_notes])
+        const accessToken =await JWT.sign(
+            {
+              
+              data: appointment_id,
+            },
+            process.env.ACCESS_TOKEN_SECRET
+          );
+        const URL = process.env.Backend_URL + "Appointment/AppointmentDetails/"+accessToken;
+        
+        //sending email
+       await sendgrid.setApiKey(process.env.SG_API_KEY)
+
+        sendgrid.send({
+            to:{
+                email: Customer_email,
+                name: Customer_First_name
+            },
+            from:{
+                email: process.env.Barbershop_Email,
+                name: 'Brothers Barber Shop Queen Marry'
+            },
+            templateId: 'd-c60813a5b72e451bad371d9a475e618d',
+            dynamicTemplateData:{
+                link: URL,
+                name: Customer_First_name
+            },
+
+        }).then(()=>
+        res.redirect(process.env.Frontend_URL+"appointment?appointment_id="+appointment_id))
+        
+    }catch (error) {
+        res.status(400).send(error)
+    }
+}
 
 module.exports={
     BarberAvailablityDates,
-    getBarberAvailablity_Hours
+    getBarberAvailablity_Hours,
+    addAppointment
+    
 }
--- barber-website/backend/routes/AppointmentRoutes.js
+++ barber-website/backend/routes/AppointmentRoutes.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const Appointment = require("../controllers/Appointment.js");
+const JWT = require("jsonwebtoken");
+
+
 
 /*
 Takes --> Barbers Id as json
@@ -31,10 +34,59 @@ returns --> res.status(200).send(availableHours.rows)
         || res.status(400).send(error)
 
 Notes and assumptions: This route will provide a list of available hours & corresponding appointment ID's when a specific date and barber is selected.
+
+THIS METHOD WILL RETURN HOURS AND APPOINTMENT_id
+{
+        "hour": "11",
+        "appointment_id": 41
+    }
+
 route: http://localhost:5001/Appointment/getBarberAvailablity_Hours
 
 */
 
 router.get('/getBarberAvailablity_Hours',Appointment.getBarberAvailablity_Hours)
+
+/*
+Takes -->  as json following info
+{
+        "appointment_id":50,
+        "Customer_First_name":"ALi",
+        "Customer_Last_name":"Ali",
+        "Customer_email":"...@gmail.com",
+        "Customer_telephone":"0000",
+        "service":"Haircut",
+        "Customer_appointment_notes":"sa"
+}
+
+returns --> res.redirect(process.env.Frontend_URL+"appointment?appointment_id="+appointment_id))
+        || res.status(400).send(error)
+
+Notes and assumptions: This route will send the email to the customers provided email (Check the spam folder as well).
+                        That email contains the link that will show all the appointment related information. 
+                        Through that link they will also be able to cancel or update existing appointments.
+                        Right now I am routing to the frontend appointment page. please change it accordingly. 
+                        In the link which redirects the user, I have attached the jwt token, which encodes the appointment ID.
+
+route: http://localhost:5001/Appointment/addAppointment
+
+*/
+
+router.put('/addAppointment',Appointment.addAppointment)
+
+//When the customer clicks the link in the email they will get routed to this page//
+router.get('/AppointmentDetails/:token', async (req,res)=>{
+        try{
+                let appointment_id = JWT.verify(req.params.token,process.env.ACCESS_TOKEN_SECRET)
+                appointment_id=appointment_id.data;
+                console.log(appointment_id)
+
+                //change accordingly 
+                res.redirect(process.env.Frontend_URL+"appointment?appointment_id="+appointment_id)
+        }catch (error) {
+                console.log(error)
+                res.status(400).send(error)
+        }
+})
 
 module.exports = router;
