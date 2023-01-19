5781a9970d80595ed0c408b6ffe7522685d3cd87
Yousha <youshahyatoolla@gmail.com>
2023-01-18 22:39:47 -0500

    UC-178 Added review function

--- barber-website/frontend/src/components/Pages/Home/Review/ReviewSec.vue
+++ barber-website/frontend/src/components/Pages/Home/Review/ReviewSec.vue
@@ -6,7 +6,18 @@
         lg="12"
         class="d-flex flex-column justify-center align-center"
       >
-        <p class="mb-3 text-h2">Reviews</p>
+        <p class="mb-1 text-h2">Reviews</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        lg="12"
+        class="d-flex flex-column justify-center align-center"
+      >
+        <v-btn @click="addReview()" class="mb-4"
+          ><v-icon size="22">mdi-plus</v-icon></v-btn
+        >
       </v-col>
     </v-row>
     <v-row class="review-sec">
@@ -21,6 +32,13 @@ import ReviewCard from "./ReviewCard.vue";
 export default {
   components: {
     ReviewCard,
+  },
+  methods: {
+    addReview() {
+      window.open(
+        "https://www.google.com/search?gs_ssp=eJwNxU0KgCAQBlDaBl0h3LTOT8SYjtAtHNMi6G8qsNvX27yyaqcWWDjBkBNd9I3ONgQCdREYHcDc6-wMJaZkgw4e8DzULPs9R7kUe-H_eT_U-cS4qdXL-wEvDhsS&q=brothers+barbershop+queen+mary&oq=broth&aqs=chrome.1.69i57j46i39i175i199j0i67i433j0i67j0i131i433i512j69i61l3.6392j0j7&sourceid=chrome&ie=UTF-8#lrd=0x4cc9197e11d611bb:0x629fb9f4c0ca11ab,3,,,,"
+      );
+    },
   },
 };
 </script>
