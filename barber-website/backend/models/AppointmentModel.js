abfe2e5e8b82358a0c307c9a455c6984c07dacbf
AQA98 <abdulqadir199853@gmail.com>
2023-01-19 19:10:56 -0500

    UC-255 Changed the SQL accordingly

--- barber-website/backend/models/AppointmentModel.js
+++ barber-website/backend/models/AppointmentModel.js
@@ -1,7 +1,7 @@
 
 const getBarberAvailablityDates= "SELECT DISTINCT available_date FROM barber_schedule WHERE UserID=$1 AND Available_Date>=$2"
 
-const getBarberAvailablity_Hours= "SELECT hour,appointment_id FROM barber_schedule WHERE UserID=$1 AND Available_Date=$2 ORDER BY hour";
+const getBarberAvailablity_Hours= "SELECT hour,appointment_id FROM barber_schedule WHERE UserID=$1 AND Available_Date=$2 and Booked=False ORDER BY hour";
 
 const addAppointment= "UPDATE barber_schedule SET booked=True, Customer_First_name = $2, Customer_Last_name = $3, Customer_email = $4, Customer_telephone=$5, service=$6, Customer_appointment_notes=$7 WHERE appointment_id = $1";
 
