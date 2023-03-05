920bffe3437d15beb311add24c78f659037e8f17
Tripe2000 <tripe2000@gmail.com>
2023-03-05 13:26:33 -0500

    uc274 updated database init file

--- ci-cd/init-db.sh
+++ ci-cd/init-db.sh
@@ -3,6 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE TYPE userRole AS ENUM ('Barber', 'Customer', 'Admin');
+
 CREATE TABLE users(
 	UserID SERIAL,
 	UserRole userRole NOT NULL,
@@ -14,8 +15,10 @@ CREATE TABLE users(
 	PRIMARY KEY (UserID),
 	UNIQUE (Telephone)
 );
+
 ALTER TABLE users
 ADD picturelink varchar(255);
+
 CREATE TABLE faq(
 	faqid SERIAL,
 	question varchar(65534) NOT NULL,
@@ -23,5 +26,57 @@ CREATE TABLE faq(
 	PRIMARY KEY (faqid)
 );
 
-INSERT INTO USERS (UserRole, Email, firstname,LastName, Telephone, Password) VALUES('Admin','abdulqadir199853@gmail.com','Abdul Qadir','Ali','1111111111','\$2b\$12\$5eTD7nTPFAH4Y3LC3wThvuLz7fHTMygKnxIsLEIafBx0ACxtVer6C');
+CREATE TABLE ResetPassword(
+	Telephone varchar(45) NOT NULL,
+	ResetCode varchar(45) NOT NULL,
+	Expiretime timestamp NOT NULL,
+	PRIMARY KEY (Telephone)
+);
+
+CREATE TYPE hour AS ENUM (
+	'10',
+	'11',
+	'12',
+	'13',
+	'14',
+	'15',
+	'16',
+	'17',
+	'18',
+	'19',
+	'20'
+);
+
+CREATE TYPE service AS ENUM (
+	'Haircut',
+	'Haircut + Beard',
+	'Line up',
+	'Beard only',
+	'Line up + Beard'
+);
+
+CREATE TABLE barber_schedule(
+	appointment_id SERIAL,
+	UserID integer REFERENCES users (userid),
+	barber_name varchar(225) NOT NULL,
+	Available_Date Date NOT NULL,
+	hour hour NOT NULL,
+	Booked Boolean DEFAULT FALSE,
+	Customer_First_name varchar(225),
+	Customer_Last_name varchar(225),
+	Customer_email varchar(225),
+	Customer_telephone varchar(225),
+	service service,
+	Customer_appointment_notes varchar(225),
+	PRIMARY KEY (appointment_id),
+	CONSTRAINT fk_users FOREIGN KEY(userid) REFERENCES users(userid)
+);
+
+CREATE TABLE products(
+    productsid SERIAL,
+    title varchar(255) NOT NULL,
+    description varchar(65534) NOT NULL,
+    picturelink varchar(255),
+    PRIMARY KEY (productsid)
+);
 EOSQL
