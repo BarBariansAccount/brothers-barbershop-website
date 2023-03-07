5b4a68039290824b22ca8fa0e718cefd27647960
Tripe2000 <tripe2000@gmail.com>
2023-03-07 16:07:49 -0500

    uc274 changed database config to point to database served address

--- barber-website/backend/config/database.js
+++ barber-website/backend/config/database.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  host: "database",
+  host: "104.225.142.153",
   database: "barbershop",
   password: "POSTGRES",
   port: 5432,
