de6e11b64d10bf9aad2b4dae684866041c5b9852
Tripe2000 <tripe2000@gmail.com>
2023-03-21 15:33:19 -0400

    uc314 changed cypress configs back to 8080 port

--- barber-website/frontend/cypress.config.js
+++ barber-website/frontend/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'vij8ht',
   e2e: {
-    baseUrl: 'http://frontend-testing:7001',
+    baseUrl: 'http://frontend-testing:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
