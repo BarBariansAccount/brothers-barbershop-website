9dd74938c4204993fb7fa008b23d1d5e23d5e9ec
Tripe2000 <tripe2000@gmail.com>
2023-03-23 13:19:39 -0400

    uc314 testing cypress on public ip

--- barber-website/frontend/cypress.config.js
+++ barber-website/frontend/cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'vij8ht',
   e2e: {
-    baseUrl: 'http://104.225.142.153:81',
+    //baseUrl: 'http://104.225.142.153:81',
+    baseUrl: 'http://www.google.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
