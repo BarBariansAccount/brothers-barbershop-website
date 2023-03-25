384c24b2485576c21b137eb2256f272ee359b05e
MansiEdi2020 <mansiedi1980@gmail.com>
2023-03-25 12:21:24 -0400

    UC-332 update Schedule and pricing on  home page

--- barber-website/frontend/src/components/Pages/Home/Services/ServicesSec.vue
+++ barber-website/frontend/src/components/Pages/Home/Services/ServicesSec.vue
@@ -1,85 +1,36 @@
 <template>
   <div>
     <v-row>
-      <v-col
-        cols="12"
-        lg="12"
-        class="d-flex flex-column justify-center align-center"
-      >
+      <v-col cols="12" lg="12" class="d-flex flex-column justify-center align-center">
         <p class="mb-3 text-h2">Our Services</p>
       </v-col>
     </v-row>
     <v-row class="haircut-types">
-      <v-col
-        cols="6"
-        lg="3"
-        class="d-flex flex-column justify-center align-center"
-      >
-        <v-card
-          class="text-center d-flex flex-column align-center justify-center rounded-xl"
-          height="85%"
-          width="80%"
-          color="#F7F7F7"
-          to="/appointment"
-        >
+      <v-col cols="6" lg="3" class="d-flex flex-column justify-center align-center">
+        <v-card class="text-center d-flex flex-column align-center justify-center rounded-xl" height="85%" width="80%"
+          color="#F7F7F7" to="/appointment">
           <img class="haircut-types-image" src="/images/hair.png" />
           <p class="font-weight-bold mt-3 text-h5">Haircut</p>
         </v-card>
       </v-col>
-      <v-col
-        cols="6"
-        lg="3"
-        class="d-flex flex-column justify-center align-center"
-      >
-        <v-card
-          class="text-center d-flex flex-column align-center justify-center rounded-xl"
-          height="85%"
-          width="80%"
-          color="#F7F7F7"
-          to="/appointment"
-        >
-          <img
-            class="haircut-types-image-2"
-            src="/images/braids.png"
-          />
+      <v-col cols="6" lg="3" class="d-flex flex-column justify-center align-center">
+        <v-card class="text-center d-flex flex-column align-center justify-center rounded-xl" height="85%" width="80%"
+          color="#F7F7F7" to="/appointment">
+          <img class="haircut-types-image-2" src="/images/braids.png" />
           <p class="font-weight-bold mt-6 text-h5">Braids</p>
         </v-card>
       </v-col>
-      <v-col
-        cols="6"
-        lg="3"
-        class="d-flex flex-column justify-center align-center"
-      >
-        <v-card
-          class="text-center d-flex flex-column align-center justify-center rounded-xl"
-          height="85%"
-          width="80%"
-          color="#F7F7F7"
-          to="/appointment"
-        >
-          <img
-            class="haircut-types-image-2"
-            src="/images/beard.png"
-          />
+      <v-col cols="6" lg="3" class="d-flex flex-column justify-center align-center">
+        <v-card class="text-center d-flex flex-column align-center justify-center rounded-xl" height="85%" width="80%"
+          color="#F7F7F7" to="/appointment">
+          <img class="haircut-types-image-2" src="/images/beard.png" />
           <p class="font-weight-bold mt-6 text-h5">Beard</p>
         </v-card>
       </v-col>
-      <v-col
-        cols="6"
-        lg="3"
-        class="d-flex flex-column justify-center align-center"
-      >
-        <v-card
-          class="text-center d-flex flex-column align-center justify-center rounded-xl"
-          height="85%"
-          width="80%"
-          color="#F7F7F7"
-          to="/appointment"
-        >
-          <img
-            class="haircut-types-image"
-            src="/images/hairdress.png"
-          />
+      <v-col cols="6" lg="3" class="d-flex flex-column justify-center align-center">
+        <v-card class="text-center d-flex flex-column align-center justify-center rounded-xl" height="85%" width="80%"
+          color="#F7F7F7" to="/appointment">
+          <img class="haircut-types-image" src="/images/hairdress.png" />
           <p class="font-weight-bold mt-3 text-h5">Line-Up</p>
         </v-card>
       </v-col>
@@ -91,18 +42,25 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
 .haircut-types {
   height: 500px;
 }
 
 .haircut-types-image {
-  height: 100px;
-  width: 100px;
+  height: 70px;
+  width: 70px;
 }
 
 .haircut-types-image-2 {
-  height: 80px;
-  width: 80px;
+  height: 70px;
+  width: 70px;
+}
+.v-card{
+  transition: all 0.4s;
+}
+.v-card:hover{
+  transform: translateY(-20px);
+/* background-color: rgba(206, 255, 194, 0.2) !important; */
 }
 </style>
