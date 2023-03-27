5b544e7587e65f61bae0c2dc48f6c64e2dd76fc7
Yousha <youshahyatoolla@gmail.com>
2023-03-27 14:51:30 -0400

    UC-334 Changed the footer to what the stakeholder wants

--- barber-website/frontend/src/components/Layouts/Default/Footer.vue
+++ barber-website/frontend/src/components/Layouts/Default/Footer.vue
@@ -1,8 +1,20 @@
 <template>
   <v-footer dark padless>
-    <v-card flat tile class="lighten-1 white--text text-center" color="#424949">
+    <v-card
+      flat
+      tile
+      class="lighten-1 white--text text-center"
+      color="#424949"
+      width="100%"
+    >
       <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+        <v-btn
+          v-for="icon in icons"
+          @click="clickIcon(icon)"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
           <v-icon size="24px">
             {{ icon }}
           </v-icon>
@@ -10,17 +22,9 @@
       </v-card-text>
 
       <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-        Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
-        accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a
-        sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
-        lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
-        iaculis dui sollicitudin. <br />
         <strong>Address:</strong> 4961c Queen Mary Rd, Montreal, Quebec H3W 1X4
         <br />
         <strong>Phone:</strong> (514) 737-7788 <br />
-        <strong>Interested in pursuing your career as a barber?</strong> Contact
-        us or come see us!
       </v-card-text>
 
       <v-divider></v-divider>
@@ -35,8 +39,15 @@
 export default {
   name: "Footer-component",
   data: () => ({
-    icons: ["mdi-facebook", "mdi-instagram"],
+    icons: ["mdi-instagram"],
   }),
+  methods: {
+    clickIcon(icon) {
+      if (icon == "mdi-instagram") {
+        window.open("https://www.instagram.com/tk_hasdat/", "_blank");
+      }
+    },
+  },
 };
 </script>
 <style></style>
